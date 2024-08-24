115c6504169c73539e611032a347de54aeb9ebac
Kevin Frankot <kevinfrankot27@gmail.com>
2024-08-23 18:38:51 -0700

    fix: support es and cjs

--- rollup.config.js
+++ rollup.config.js
@@ -13,10 +13,16 @@ Kevin Frankot - <%= pkg.name %> v<%= pkg.version %>
 export default [
   {
     input: 'src/index.ts',
-    output: {
-      file: 'dist/index.js',
-      format: 'es',
-    },
+    output: [
+      {
+        file: 'dist/index.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+      },
+    ],
     external: ['yup'],
     plugins: [
       license({
